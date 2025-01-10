1d668d7a96bbd2250cf32e6f5c2b69bab4784d7d
indrareddy15 <gantaindrareddy83@gmail.com>
2025-01-10 09:53:38 +0530

    auth user model

---
+++ model/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
